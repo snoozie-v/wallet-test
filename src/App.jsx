ee3eac8a891b734c2338e290e20c9b252ebaa9e5
snoozie-v <gettingsleepyz@gmail.com>
2023-10-22 08:31:28 -0700

    pagination and sort add

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ const connex = new Connex({
   network: "main"
 });
 
+
+
 export default function App() {
   const [certificate] = useState({
     purpose: "identification",
@@ -17,10 +19,15 @@ export default function App() {
       content: "content to sign"
     }
   });
-  
+  const [userWallet, setUserWallet] = useState(null);
   const [nftImages, setNftImages] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [isLoading, setIsLoading] = useState(false); // Loading state
+
+  const limit = 4;
 
   const handleSigning = async () => {
+    setIsLoading(true)
     try {
       const signedMessage = await connex.vendor
         .sign("cert", certificate)
@@ -28,75 +35,106 @@ export default function App() {
 
       const userWallet = signedMessage.annex.signer;
       console.log(userWallet);
-
-
-
-      await fetchNFTs(userWallet);
-    } catch (err) {}
+      setUserWallet(userWallet)
+      const newImages = await fetchNFTs(userWallet, offset, limit);
+      setNftImages(newImages)
+    } catch (err) {
+      console.log("there was an error")
+    } finally {
+      setIsLoading(false)
+    }
   };
 
+  const handleLoadMore = async () => {
+    setIsLoading(true)
+    const newOffset = offset + limit;
+    const newLimit = limit;
+    const newImages = await fetchNFTs(userWallet, newOffset, newLimit);
+    setNftImages([...nftImages, ...newImages]);
+    setIsLoading(false);
+    setOffset(newOffset)
+  };
 
+  const sortNFTsByTokenID = () => {
+    const sortedNftImages = [...nftImages].sort((a, b) => a.tokenId - b.tokenId);
+    setNftImages(sortedNftImages);
+  };
 
-  async function fetchNFTs(userWallet) {
-    const MINOS = connex.thor.account(
-      "0xf4d82631be350c37d92ee816c2bd4d5adf9e6493"
-    );
+async function fetchNFTs(userWallet, offset, limit) {
+  const MINOS = connex.thor.account("0xf4d82631be350c37d92ee816c2bd4d5adf9e6493");
 
-    const balance = MINOS.method(balanceOf);
-    const nftID = MINOS.method(tokenOfOwnerByIndex);
-    const nftURI = MINOS.method(TokenURIAbi);
+  const balance = MINOS.method(balanceOf);
+  const nftID = MINOS.method(tokenOfOwnerByIndex);
+  const nftURI = MINOS.method(TokenURIAbi);
 
-    const output = await balance.call(userWallet);
-    const balanceValue = output.decoded[0];
+  const output = await balance.call(userWallet);
+  const balanceValue = output.decoded[0];
 
-    if (balanceValue > 0) {
-      let images = [];
-      for (let i = 0; i < balanceValue; i++) {
-        const nftIDOutput = await nftID.call(userWallet, i);
-        const tokenId = nftIDOutput.decoded[0];
+  if (balanceValue > 0) {
+    let images = [];
 
-        const URIOutput = await nftURI.call(tokenId);
+    // Implement pagination (adjust offset and limit as needed)
+    for (let i = offset; i < Math.min(offset + limit, balanceValue); i++) {
+      const nftIDOutput = await nftID.call(userWallet, i);
+      const tokenId = nftIDOutput.decoded[0];
 
-        const metadataResponse = await fetch(
-          `https://arweave.net/${URIOutput.decoded[0].substr(5)}`
-        );
-  
-        const metadata = await metadataResponse.json();
-  
-        const imageUrl = metadata.image;
-        const presentImage = await fetch(`https://arweave.net/${imageUrl.substr(5)}`);
-        const presentImageURL = presentImage.url;
+      const URIOutput = await nftURI.call(tokenId);
 
-        images.push({ imageUrl: presentImageURL, tokenId: tokenId });
-      }
-      console.log(images);
-      setNftImages(images);
-      return images;
+      const metadataResponse = await fetch(`https://arweave.net/${URIOutput.decoded[0].substr(5)}`);
+
+      const metadata = await metadataResponse.json();
+
+      const imageUrl = metadata.image;
+      const presentImage = await fetch(`https://arweave.net/${imageUrl.substr(5)}`);
+      const presentImageURL = presentImage.url;
+
+      images.push({ imageUrl: presentImageURL, tokenId: tokenId });
     }
-    return [];
+
+    console.log(images);
+    setNftImages(images);
+    return images;
   }
-
-  return (
-<div>
-
-
-<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
-<button onClick={handleSigning}>view Minos by signing</button>
-  <img src={mino}></img>
-  <h1 style={{width: '100%', margin: '0', padding: '0'}}>View OG Minos</h1>
-  
-    {nftImages.map((image, index) => (
-     <div key={index} style={{ flex: '1', maxWidth: 'calc(25% - 10px)' }}>
-    <img
-      key={index}
-      src={image.imageUrl}
-      alt={`NFT ${index}`}
-      style={{ width: '100%', height: 'auto', paddingTop: '50px', minWidth: '250px' }}
-    />
-    <p style={{ textAlign: 'center' }}>Id: {image.tokenId}</p>
-    </div>
-  ))}
-</div>
-</div>
-  );
+  return [];
 }
+
+
+return (
+  <div>
+    <div className='header'>
+      <div className='socials'>
+        <div className='social discord'>D</div>
+        <div className='social discord'>I</div>
+        <div className='social x'>X</div>
+      </div>
+      <img className='logo' src={mino}></img>
+      <button className='wallet button' onClick={handleSigning}>
+        view Minos by signing
+      </button>
+      <div className='navMenu'>Nav</div>
+    </div>
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+      <h1 style={{ width: '100%', margin: '0', padding: '25px' }}>View OG Minos</h1>
+      {isLoading && nftImages.length === 0 ? (
+        <p>Loading...</p>
+      ) : null}
+
+      {nftImages && nftImages.length > 0 ? (
+        nftImages.map((image, index) => (
+          <div key={index} style={{ flex: '1', maxWidth: 'calc(25% - 10px)' }}>
+            <img
+              key={index}
+              src={image.imageUrl}
+              alt={`NFT ${index}`}
+              style={{ width: '100%', height: 'auto', paddingTop: '50px', minWidth: '250px' }}
+            />
+            <p style={{ textAlign: 'center' }}>Id: {image.tokenId}</p>
+          </div>
+        ))
+      ) : null}
+    </div>
+    <button onClick={handleLoadMore}>Load More</button>
+    <button onClick={sortNFTsByTokenID}>Sort by Token ID</button> {/* Add a button to trigger sorting */}
+  </div>
+);
+        }
